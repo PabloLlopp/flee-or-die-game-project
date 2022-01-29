6413495666217c3fea814c415eb99fbe65363e30
Pablo López <p.lopezmunoz@gmail.com>
2022-01-29 13:40:54 +0100

    add music

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,21 @@
+window.onload = () =>{
+    const audio = new Audio()
+    audio.src = '../music/tune.mp3'
+    audio.type = 'audio/wav'
+    var playPromise = audio.play();
+
+    if (playPromise !== undefined) {
+        playPromise.then(function () {
+            console.log('Playing....');
+        }).catch(function (error) {
+            console.log('Failed to play....' + error);
+        });
+    }
+}
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d')
 
 canvas.height = 500
 canvas.width = 500
+
