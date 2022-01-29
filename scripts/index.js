5cacdd1c84e65d037bc504ba13eb6f5830ed78d9
Pablo López <p.lopezmunoz@gmail.com>
2022-01-29 13:43:44 +0100

    add music

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,5 @@
 window.onload = () =>{
-    const audio = new Audio()
-    audio.src = '../music/tune.mp3'
-    audio.type = 'audio/wav'
-    var playPromise = audio.play();
-
-    if (playPromise !== undefined) {
-        playPromise.then(function () {
-            console.log('Playing....');
-        }).catch(function (error) {
-            console.log('Failed to play....' + error);
-        });
-    }
+    
 }
 
 const canvas = document.getElementById('canvas');
@@ -19,3 +8,7 @@ const ctx = canvas.getContext('2d')
 canvas.height = 500
 canvas.width = 500
 
+const audio = new Audio('../music/tune.mp3')
+    audio.src = '../music/tune.mp3'
+    audio.type = 'audio/wav'
+    audio.play()
