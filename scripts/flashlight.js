07bc2dd661a6b8ceefa889f313db676e94a0633c
Pablo López <p.lopezmunoz@gmail.com>
2022-02-03 12:24:26 +0100

    Formatting

--- scripts/flashlight.js
+++ scripts/flashlight.js
@@ -1,43 +1,43 @@
 class Flashlight {
-    constructor(ctx){
-        this.ctx = ctx;
-        this.light = [];
-        this.width = 10;
-        this.height = 10;
-        this.player = player;
-        this.rotation = 0;
-        this.moveX = 10;
-        this.moveY = 10;
-        this.lightOn = new Image();
-        this.lightOn.src = 'images/flashlight-on.png'
-        this.lightOff = new Image();
-        this.lightOff.src = 'images/flashlight-off.png'
-    }
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.light = [];
+    this.width = 10;
+    this.height = 10;
+    this.player = player;
+    this.rotation = 0;
+    this.moveX = 10;
+    this.moveY = 10;
+    this.lightOn = new Image();
+    this.lightOn.src = "images/flashlight-on.png";
+    this.lightOff = new Image();
+    this.lightOff.src = "images/flashlight-off.png";
+  }
 
-    turnOnLight(){
-        if (this.light.length < 1){
-        this.light.push(
-            {
-            x: this.player.x ,
-            y: this.player.y ,
-            width: this.width,
-            height: this.height
-            }
-        )}
-        return true
+  turnOnLight() {
+    if (this.light.length < 1) {
+      this.light.push({
+        x: this.player.x,
+        y: this.player.y,
+        width: this.width,
+        height: this.height,
+      });
     }
+    return true;
+  }
 
-    draw(){
-        
-        if (this.turnOnLight){
-            this.ctx.save();
-            this.ctx.translate(
-                this.light[0].x, this.light[0].y
-            )
-            this.ctx.rotate(this.rotation)
-            this.ctx.drawImage(this.lightOn, -this.lightOn.width / 2 , -this.lightOn.height + 20 )
-            this.ctx.restore();
-        }/* else {
+  draw() {
+    if (this.turnOnLight) {
+      this.ctx.save();
+      this.ctx.translate(this.light[0].x, this.light[0].y);
+      this.ctx.rotate(this.rotation);
+      this.ctx.drawImage(
+        this.lightOn,
+        -this.lightOn.width / 2,
+        -this.lightOn.height + 20
+      );
+      this.ctx.restore();
+    } /* else {
             this.ctx.save();
             this.ctx.translate(
                 this.light[0].x, this.light[0].y
@@ -46,47 +46,44 @@ class Flashlight {
             this.ctx.drawImage(this.lightOff, -this.lightOff.width / 2 , -this.lightOff.height + 20 )
             this.ctx.restore();
         }*/
-    }
+  }
 
-    lightRotation(){
-        this.rotation = Math.atan2(-(this.player.x - mouse.x), ( this.player.y - mouse.y))
-        //console.log(this.rotation)
-    }
+  lightRotation() {
+    this.rotation = Math.atan2(
+      -(this.player.x - mouse.x),
+      this.player.y - mouse.y
+    );
+    //console.log(this.rotation)
+  }
 
-    move(key){
-        this.lightRotation();
-        
-        if (moveUp)
-            if (this.light[0].y > 10)
-                this.light[0].y -= this.moveY
-        if (moveDown)
-            if (this.light[0].y + this.height < canvas.height) 
-                this.light[0].y += this.moveY
-                
-        if (moveLeft)
-            if (this.light[0].x > 10)
-                this.light[0].x -= this.moveX
-                
-        if (moveRight)
-            if (this.light[0].x + this.width < canvas.width)
-                this.light[0].x = this.light[0].x + this.moveX
-                
-    }
+  move(key) {
+    this.lightRotation();
 
-    left() {
-        return this.light[0].x ;
-    }
+    if (moveUp) if (this.light[0].y > 10) this.light[0].y -= this.moveY;
+    if (moveDown)
+      if (this.light[0].y + this.height < canvas.height)
+        this.light[0].y += this.moveY;
 
-    right() {
-        return this.light[0].x + this.width + 43 * this.rotation;
-    }
+    if (moveLeft) if (this.light[0].x > 10) this.light[0].x -= this.moveX;
 
-    top() {
-        return this.light[0].y;
-    }
+    if (moveRight)
+      if (this.light[0].x + this.width < canvas.width)
+        this.light[0].x = this.light[0].x + this.moveX;
+  }
 
-    bottom() {
-        return this.light[0].y + this.height + 43 * this.rotation;
-    }
+  left() {
+    return this.light[0].x;
+  }
 
+  right() {
+    return this.light[0].x + this.width + 43 * this.rotation;
+  }
+
+  top() {
+    return this.light[0].y;
+  }
+
+  bottom() {
+    return this.light[0].y + this.height + 43 * this.rotation;
+  }
 }
--- scripts/walls.js
+++ scripts/walls.js
@@ -1,31 +1,26 @@
 class Walls {
-    constructor(ctx){
-        this.ctx = ctx;
-        this.allWalls =[];
-        this.width = 50
-        this.height = 50
-    
-    }
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.allWalls = [];
+    this.width = 50;
+    this.height = 50;
+  }
 
-    addRandomWalls(){
-        for (let i = 0; i < 10; i++){
-            this.allWalls.push(
-                {
-                x: Math.floor(Math.random() * this.ctx.canvas.width) - this.width,
-                y: Math.floor(Math.random() * this.ctx.canvas.height) - this.height,
-                width: this.width,
-                height: this.height
-                }
-            )
-        }        
+  addRandomWalls() {
+    for (let i = 0; i < 10; i++) {
+      this.allWalls.push({
+        x: Math.floor(Math.random() * this.ctx.canvas.width) - this.width,
+        y: Math.floor(Math.random() * this.ctx.canvas.height) - this.height,
+        width: this.width,
+        height: this.height,
+      });
     }
+  }
 
-    draw(){
-        this.allWalls.forEach(item=>{
-            this.ctx.fillStyle = 'black';
-            this.ctx.fillRect(item.x, item.y, item.width, item.height)
-            }
-        )       
-    }
-
+  draw() {
+    this.allWalls.forEach((item) => {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(item.x, item.y, item.width, item.height);
+    });
+  }
 }
